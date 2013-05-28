33b064bbe50ef52709f3db8e9dd1fa40f6096e55
robert <robert@robert-Lenovo-Product.(none)>
2013-05-28 23:19:43 +0800

    c files update

--- src/log.c
+++ src/log.c
@@ -7,16 +7,33 @@
 
 #include "log.h"
 
+#include <stdarg.h>
+
+int32_t enable_log( int32_t iIsEnable )
+{
+  int32_t iRetCode = -1;
+
+  return iRetCode;
+}
+
+int32_t set_log( const C_LOG_TYPE eLogType, const void *pParam, const int32_t iParamSize )
+{
+  int32_t iRetCode = -1;
+
+  return iRetCode;
+}
+
 int32_t log_print( const int8_t *pFormat, ... )
 {
 	int32_t iRetCode = -1;
 	va_list args;
-	int8_t pTempbuf[1024] = { 0x00, };
+	int8_t pTempBuf[1024] = { 0x00, };
 	
-    va_start(args, pFormat);
-    iRetCode = vsprintf(pTempbuf, pFormat, args);
-    va_end(args);
+	va_start(args, pFormat);
+	iRetCode = vsnprintf(pTempBuf, sizeof(pTempBuf), pFormat, args);
+	va_end(args);
 	fwrite( pTempBuf, 1, iRetCode, stdout );
+	fwrite( "\r\n", 1, 2, stdout );
 	
 	return iRetCode;
 }
