3e9da4ccd497aa8d3ba7a9f69eb0ab4ddf969014
robert <robert@robert-Lenovo-Product.(none)>
2013-05-28 17:40:54 +0800

    config update

---
+++ inc/config.h
@@ -0,0 +1,32 @@
+/********************************************************
+ * file name: config.h
+ * writer: gzl
+ * date time:2013-05-28 17:29
+ * description: config file for framework kpf
+ * *****************************************************/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#ifdef (__cplusplus)
+extern "C"
+{
+#endif
+
+/*************************************************
+ * OS Type Config.
+ * ***********************************************/
+#define __OS_WIN32__  (1)//is windows platform.
+#define __OS_LINUX__  (0)//is linux platform.
+#define __OS_MAC__  (0)//is mac platform.
+#define __OS_IOS__  (0)//is ios platform.
+#define __OS_ANDROID__  (0)//is android platform.
+
+//If enable debug support.
+#define DEBUG_SUPPORT  (1)
+	
+#ifdef (__cplusplus)
+}
+#endif
+
+#endif/* __CONFIG_H__ */
