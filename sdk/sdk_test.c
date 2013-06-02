3f6f394f045e8f341b0ad4e5c1a29d76cd62d421
robert <robert@robert-Lenovo-Product.(none)>
2013-06-02 11:36:22 +0800

    pine test update

--- sdk/sdk_test.c
+++ sdk/sdk_test.c
@@ -12,12 +12,90 @@ typedef struct CPerson_t
 
 	int32_t (*on_destory_child)( void *pPerson );
 
+	int32_t m_iHasPersonChild;		
+
 	int32_t m_iAge;//age of person;
 	int8_t m_btSex[2];//sex of person;
 	int32_t m_btId[32];//identification of person;
 }CPerson;
 
+#define EXTENDS_PERSON \
+	EXTENDS_PINE \
+	int32_t (*on_destory_child)( void *pPerson ); \
+	int32_t m_iHasPersonChild;	\
+	int32_t m_iAge; \
+	int8_t m_btSex[2]; \
+	int32_t m_btId[32]; \
+
+#define CHILD_ADDR_OF_PERSON(person) \
+		(( int8u_t * )( ((int8u_t *)person) + sizeof( CPerson ) ))
+
+typedef struct CChinese_t
+{
+	EXTENDS_PERSON
+
+	int32_t (*on_destory_chinese)( void *pChinese );
+	
+	int8_t m_btNationality[32];
+		
+}CChinese;
+
+int32_t release_chinese( void *pChinese );
+
 int32_t release_person( void *pPerson );
+
+CChinese *create_chinese( void )
+{
+	CChinese *pRetCode = NULL;
+	
+	log_print( "create_chinese:--------------------->" );
+	pRetCode = mem_malloc( sizeof(*pRetCode) );
+	if ( pRetCode )
+	{
+		if ( operator_init( (CPine *)pRetCode ) >= 0 )
+		{
+			pRetCode->on_destory_child = release_person;
+
+			pRetCode->on_destory_chinese = release_chinese;
+
+			pRetCode->m_btNationality[0] = '1';
+			
+			pRetCode->m_iHasChild = 1;
+			pRetCode->m_iHasPersonChild = 1;
+		}
+		else
+			log_print( "init chinese base pine failed???????????????" );
+	}
+	else
+		log_print( "malloc chinese failed????????????" );
+
+	log_print( "create_chinese<----------------------------" );
+
+	return pRetCode;
+}
+
+int32_t release_chinese( void *pChinese )
+{
+	int32_t iRetCode = -1;
+	
+	log_print( "release_chinese:------------------->" );	
+	if ( pChinese )
+	{
+		CChinese *pCh = (CChinese *)pChinese;
+
+		log_print( "pCh->%u", pCh );
+
+		//mem_free( pCh );
+		//pCh = NULL;
+	}
+	else
+		log_print( "release_chinese:param is NULL." );
+
+	log_print( "release_chinese<-------------------------" );
+
+	return iRetCode;
+}
+
 CPerson *create_person( void )
 {
 	CPerson *pRetCode = NULL;
@@ -36,6 +114,8 @@ CPerson *create_person( void )
 			pRetCode->m_btSex[1] = '\0';
 			pRetCode->m_btId[0] = '\0';
 
+			pRetCode->m_iHasChild = 0;
+
 			log_print( "pPerson-->%u", pRetCode );
 		}
 	}
@@ -53,6 +133,18 @@ int32_t release_person( void *pPerson )
 
 	if ( pPerson )
 	{
+		CPerson *pPer = (CPerson *)pPerson;
+		
+		int32_t (*on_destory_person_child)( void *pChild ) = NULL;
+		
+		if ( pPer->m_iHasPersonChild )
+		{
+			memcpy( &on_destory_person_child, CHILD_ADDR_OF_PERSON(pPerson), sizeof(on_destory_person_child) );		
+	
+			if ( on_destory_person_child )
+				on_destory_person_child( pPer );
+		}
+
 		mem_free( pPerson );
 
 		pPerson = NULL;
@@ -64,11 +156,26 @@ int32_t release_person( void *pPerson )
 int main( int argc, char **argv )
 {
 	//CQueue queue;
-	CPerson *pMe = NULL;
+	//CPerson *pMe = NULL;
+
+	CChinese *pMe = NULL;
 
 	enable_log( 1 );
 	set_log( LOG_TYPE_CONSOLE, NULL, 0 );
-	
+
+		
+	pMe = create_chinese( );
+	if ( pMe )
+	{
+		CChinese *pMe2 = (CChinese *)operator_den( (CPine *)pMe );
+		if ( pMe2 )
+		{
+			operator_release( (CPine *)pMe2 );
+		}
+
+		operator_release( (CPine *)pMe );
+	}
+#if 0
 	pMe = create_person();
 	if ( pMe )
 	{
@@ -88,7 +195,7 @@ int main( int argc, char **argv )
 	else
 		log_print( "pMe is NULL." );
 	//log_print( "Hello world");
-
+#endif
 #if 0
 	if ( init_queue( &queue ) >= 0 )
 	{
