ebe5623f6f207e3329d9b6ebf97d4b1819a3b5e4
robert <robert@robert-Lenovo-Product.(none)>
2013-06-02 22:04:23 +0800

    btree update

--- inc/btree.h
+++ inc/btree.h
@@ -8,13 +8,51 @@
 #ifndef __BTREE_H__
 #define __BTREE_H__
 
-#ifdef (__cplusplus)
+#include "config.h"
+#include "typedefs.h"
+#include "lock.h"
+
+#if defined(__cplusplus)
 extern "C"
 {
 #endif
 
+typedef struct CBTree_t
+{
+	void *pBTreeTbl;
+	
+	CMutex Locker;
+}CBTree;
 
-#ifdef (__cplusplus)
+typedef struct CBTreeNode_t
+{
+	void *pBTNode;	
+
+	void *pData;
+}CBTreeNode;
+
+//compare type.
+typedef int32_t (*btree_comp_t)( const void *pValA, const void *pValB, void *pParam );
+
+//avl value type.
+typedef void (*btree_node_t)( void *pBTreeNode, void *pParam );
+
+//copy avl node.
+typedef void *(*btree_node_copy_t)( void *pBTreeNode, void *pParam );
+
+
+//create btree.
+CBTree *create_btree( btree_comp_t comp );
+//destory btree.
+int32_t destory_btree( CBTree *pBTree );
+//add node to btree.
+int32_t add_btree_node( CBTree *pBTree, CBTreeNode *pBTreeNode );
+//remove btree node.
+CBTreeNode *remove_btree_node( CBTree *pBTree, int32u_t iBTNodeId );
+//search btree node.
+CBTreeNode *search_btree_node( CBTree *pBTree, int32u_t iBTNodeId );
+
+#if defined(__cplusplus)
 }
 #endif
 
