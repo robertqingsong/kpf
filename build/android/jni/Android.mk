257e132777cfa1e0089ee9e7e5caf6b370e4a674
robert <robert@robert-Lenovo-Product.(none)>
2013-06-09 11:13:12 +0800

    android makefile update

---
+++ build/android/jni/Android.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := kpf
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) ../../../inc/ ../../../src/
+
+LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
+
+LOCAL_SRC_FILES := ../../../src/list.c \
+						../../../src/avl.c \
+						../../../src/btree.c \
+						../../../src/hash.c \
+						../../../src/log.c \
+						../../../src/pine.c \
+						../../../src/kqueue.c \
+						../../../src/mem_api.c \
+						../../../src/queue.c \
+						../../../src/task.c \
+						../../../src/block.c \
+						../../../src/epoll.c \
+						../../../src/net_api.c \
+						../../../src/select.c \
+						../../../src/timer.c \
+						../../../src/bsearch.c \
+						../../../src/future.c \
+						../../../src/lock.c \
+						../../../src/oal_api.c \
+						../../../src/sem.c
+
+include $(BUILD_SHARED_LIBRARY)
